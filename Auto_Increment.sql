f181ed85aef04d269d33c3037a6a6b9bc7c109c6
N_P <naseemapathan879@gmail.com>
2024-10-24 17:45:01 +0530

    Create Auto_Increment.sql

---
+++ Auto_Increment.sql
@@ -0,0 +1,17 @@
+CREATE TABLE animals (  
+     id MEDIUMINT NOT NULL AUTO_INCREMENT,  
+     name CHAR(30) NOT NULL,  
+     PRIMARY KEY (id)  
+);  
+INSERT INTO animals (name) VALUES  
+    ('dog'),('cat'),('penguin'),  
+    ('lax'),('whale'),('ostrich');  
+SELECT * FROM animals;  
+
+INSERT INTO animals (id,name) VALUES(0,'groundhog');  
+
+INSERT INTO animals (id,name) VALUES(100,'rabbit');  
+
+INSERT INTO animals (id,name) VALUES(NULL,'mouse');  
+
+SELECT * FROM animals;  
